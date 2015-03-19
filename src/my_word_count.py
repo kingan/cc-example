093ef049d8750157a26ecf11c82597747a480bca
Andrew King <kingan@tcd.ie>
2015-03-18 21:36:11 -0400

    Update my_word_count.py

--- src/my_word_count.py
+++ src/my_word_count.py
@@ -1,1 +1,35 @@
+from nltk import word_tokenize
+import time
+import multiprocessing as mp
+import os, subprocess
+import string
+from collections import Counter
+
+data_files_output = subprocess.check_output(["ls","/home/aking/p/split_bible"])
+d = data_files_output.split()
+def my_fun(i):
+    input = "/home/aking/p/split_bible/" + i
+    f = open(input,'r')
+    out = f.read().translate(string.maketrans("",""), string.punctuation)
+    wordcount = Counter(word_tokenize(out.lower()))
+    return wordcount
+
+bert = map(my_fun,d)
+ernie = reduce( (lambda x,y:x+y), bert)
+arc = ernie.items()
+arc.sort()
+
+output = ''
+for item in arc:
+    if(len(item[0]) <= 7):
+        output += item[0] + "\t\t" + str(item[1])
+    else:
+        output += item[0] + "\t" + str(item[1])
+    output += "\n"
+
+out = open('wc_result.txt', 'w')
+out.write(output)
+out.close()
+
+
 
