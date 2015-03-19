fd467a34ee4b098374cc05eef1255dc87fb1cd2b
Andrew King <kingan@tcd.ie>
2015-03-19 09:23:28 -0400

    Update my_word_count.py

--- src/my_word_count.py
+++ src/my_word_count.py
@@ -1,14 +1,14 @@
 from nltk import word_tokenize
 import time
 import multiprocessing as mp
-import os, subprocess
+import os, subprocess, sys
 import string
 from collections import Counter
 
-data_files_output = subprocess.check_output(["ls","/home/aking/p/split_bible"])
+data_files_output = subprocess.check_output(["ls",sys.argv[1]])
 d = data_files_output.split()
 def my_fun(i):
-    input = "/home/aking/p/split_bible/" + i
+    input = sys.argv[1] + i
     f = open(input,'r')
     out = f.read().translate(string.maketrans("",""), string.punctuation)
     wordcount = Counter(word_tokenize(out.lower()))
@@ -27,7 +27,7 @@ for item in arc:
         output += item[0] + "\t" + str(item[1])
     output += "\n"
 
-out = open('wc_result.txt', 'w')
+out = open(sys.argv[2], 'w')
 out.write(output)
 out.close()
 
