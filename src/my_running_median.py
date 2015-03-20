efe7247b7a665cb94b2519b44eba6804996c6ffb
Andrew King <kingan@tcd.ie>
2015-03-20 16:50:35 -0400

    Update my_running_median.py

--- src/my_running_median.py
+++ src/my_running_median.py
@@ -1,13 +1,12 @@
-from heapq import heappush, heappop
+from heapq import heappop, heappush, heappushpop
 import subprocess
 import string
 import sys
 
-#Create a sorted list of file names.
+#Create a sorted list of file names
 word_files_output = subprocess.check_output(["ls",sys.argv[1]])
 word_files_list = word_files_output.split()
 word_files_list.sort()
-
 
 #Define a function to return the number of words on each line
 def my_fun(i):
@@ -18,32 +17,29 @@ def my_fun(i):
     numbers = map(lambda x: len(x.split()),out)
     return numbers
 
-#Execute the map function.
-#Reduce it to a single list
-arou = map(my_fun,word_files_list)
-numbers = reduce((lambda x,y:x+y),arou)
-
-#with open('/home/aking/p/raw_files/'+i,'r') as f:
-#      output = f.read().translate(string.maketrans("",""), string.punctuation)
-
-
-from heapq import heappop, heappush, heappushpop
-
-def streaming_median(seq):
+#Define the running median function using a binary tree
+def running_median(seq):
     seq = iter(seq)
     left, right  = [], [next(seq)]
     while True:
-        yield right[0] if len(right) > len(left) else (right[0] - left[0])/2.0
-
+        yield float(right[0]) if len(right) > len(left) else (right[0] - left[0])/2.0
         heappush(left, -heappushpop(right, next(seq)))
         if len(left) > len(right):
             heappush(right, -heappop(left))
 
+try:
+    #Execute the map function and reduce the output to a single stream of numbers
+    map_result = map(my_fun,word_files_list)
+    numbers = reduce((lambda x,y:x+y),map_result)
 
-a = [float(item) for item in streaming_median(numbers)]
-b = map((lambda x:str(x)+"\n"),a)
-b = reduce((lambda x,y: x+y),b)
+    #Execute the running_median function.
+    running_median_result = [item for item in running_median(numbers)]
+    formatted_result = map((lambda x:str(x)+"\n"),running_median_result)
+    formatted_result = reduce((lambda x,y: x+y),formatted_result)
+except:
+    print "Could not execute function."
 
+#Output the running median.
 out = open(sys.argv[2], 'w')
-out.write(b)
+out.write(formatted_result)
 out.close()
