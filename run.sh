8087f0b3cca4be2a5a0203b96f23ddcfd24e28a0
Andrew King <kingan@tcd.ie>
2015-03-20 15:27:30 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ chmod a+x ./src/my_running_median.py
 
 # finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
 python ./src/my_word_count.py ./wc_input ./wc_output/wc_result.txt
-python ./src/my_running_median.py ./wc_input ./wc_output/med_result.txt
+python ./src/my_running_median.py ./wc_input/ ./wc_output/med_result.txt
 
